2dd0a391b3c8f2f03f5d9bda8873f1c7b4c5b686
NullCodex <jamesonyu95@gmail.com>
2015-12-08 21:19:53 -0500

    Got rid of some unused modules

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var port = process.env.PORT || 8080;
 var express = require('express');
 var request = require('request');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
