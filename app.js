6c374c31a05be84a66d351b9ae25622a77ecd91a
NullCodex <jamesonyu95@gmail.com>
2015-12-08 20:50:43 -0500

    Initial push

---
+++ app.js
@@ -0,0 +1,20 @@
+require('newrelic');
+
+var app = require('express')();
+var port = process.env.PORT || 8080;
+var express = require('express');
+var request = require('request');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.get('/main', function(req, res){
+
+  res.sendFile(path.join(__dirname, 'views', 'main.html'));
+});
